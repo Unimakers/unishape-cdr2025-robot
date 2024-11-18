f148a75cf0442a01f308530de6c7e662fa833ca3
GZod01 <gzod01@gzod01.fr>
2024-11-18 18:07:21 +0100

    Ajout de fichiers de configuration et de bibliothèques pour le projet PlatformIO

---
+++ include/ROBOT_VARIABLES.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#ifndef ROBOT_VARIABLES_H
+#define ROBOT_VARIABLES_H
+#endif 
+
+#define TEAM_NAME "UniGaffer"
+
+namespace PIN
+{
+    namespace STEPPERS
+    {
+        constexpr int 
+                    LEFT_STEP = 2,
+                    RIGHT_STEP = 3,
+                    LEFT_DIR = 4,
+                    RIGHT_DIR = 5;
+    }
+} // namespace PIN
+
+#define WHEEL_DIAMETER 0.06 // 0.06 m = 6cm
+#define STEPPER_ANGLE 1.8 // 1.8 degrees per step
+#define STEPPER_STEPS_PER_REV 200 // 200 steps per revolution
---
+++ lib/RobotMove/RobotMove.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <AccelStepper.h>
+#include <ROBOT_VARIABLES.h>
+
+#ifndef ROBOTMOVE_H
+#define ROBOTMOVE_H
+#endif
+
