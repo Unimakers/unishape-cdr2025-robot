31150c619b38a129128c5cfa461b3a2f5d4fe70a
gzod01 <gzod01@gzod01.fr>
2025-04-20 03:36:23 +0200

    les steppers marchent pas.... je vais finir par m'endormir par terre

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <Arduino.h>
+
+#define BONJOUR 0
+#if BONJOUR == 0
+// DEBUG
+// ça marche pas...
 #include <RobotMove.hpp>
 RobotMove rob;
 void setup() {
@@ -12,60 +17,67 @@ void setup() {
 void loop() {
   Serial.println("Loop running");
   rob.Run();
+  
   delay(1000);
 }
-// #include <lidar.c++>
-// #include <ActionHandle.hpp>
-// #include <ActionListes.hpp>
-// #include <string>
+// FIN DEBUG
+#else
 
-// ActionHandle actionHandle;
 
-// enum struct G_STATE
-// {
-//     INITIALIZING,
-//     LOOPING
-// };
-// G_STATE gState=G_STATE::INITIALIZING;
 
-// // put function declarations here:
-// //
-// // int startTime=0;
-// void setup()
-// {
-//     gState=G_STATE::INITIALIZING;
-//     Serial.begin(115200);
-//     delay(1000);
-//     Serial.println("Préinitialisation");
-//     actionHandle = ActionHandle();
-//     ActionList actionList = ActionList();
-//     actionList.pushAction(&actionHandle);
-//     Serial.println(((std::string)"final op : "+std::to_string(actionHandle.actions.size())).c_str());
-//     tone(12, 444, 250);
-//     initLidar();
-//     // lidarSerial.begin(115200, 134217756U, RX, TX);
-//     //
-//     delay(500);
-//     pinMode(PIN::DIVERS::TIRETTE, INPUT_PULLUP);
-//     tone(12, 300, 250);
-//     actionHandle.waitTirette();
-//     actionHandle.initRobot();
-//     Serial.println("Fin initialisation");
-//     actionHandle.setState(ActionHandle::STATE::IDLE);
-//     gState=G_STATE::LOOPING;
-//     Serial.println("Vraiment fin init");
-//     // startTime= millis();
-// }
-// void loop(){
+#include <lidar.c++>
+#include <ActionHandle.hpp>
+#include <ActionListes.hpp>
+#include <string>
 
-//     Serial.println("err1");
-//     if(gState!=G_STATE::LOOPING) return;
-//     Serial.println("err2");
-//     if (getLidarStatus())
-//     {
-//         actionHandle.setState(ActionHandle::STATE::PAUSED);
-//     }
-//     Serial.println("err3");
-//     actionHandle.actionLoop();
-//     delay(250);
-// }
+ActionHandle actionHandle;
+
+enum struct G_STATE
+{
+    INITIALIZING,
+    LOOPING
+};
+G_STATE gState=G_STATE::INITIALIZING;
+
+// put function declarations here:
+//
+// int startTime=0;
+void setup()
+{
+    gState=G_STATE::INITIALIZING;
+    Serial.begin(115200);
+    delay(1000);
+    Serial.println("Préinitialisation");
+    actionHandle = ActionHandle();
+    ActionList actionList = ActionList();
+    actionList.pushAction(&actionHandle);
+    Serial.println(((std::string)"final op : "+std::to_string(actionHandle.actions.size())).c_str());
+    tone(12, 444, 250);
+    initLidar();
+    // lidarSerial.begin(115200, 134217756U, RX, TX);
+    //
+    delay(500);
+    pinMode(PIN::DIVERS::TIRETTE, INPUT_PULLUP);
+    tone(12, 300, 250);
+    actionHandle.waitTirette();
+    actionHandle.initRobot();
+    Serial.println("Fin initialisation");
+    actionHandle.setState(ActionHandle::STATE::IDLE);
+    gState=G_STATE::LOOPING;
+    Serial.println("Vraiment fin init");
+    // startTime= millis();
+}
+void loop(){
+
+    Serial.println("err1");
+    if(gState!=G_STATE::LOOPING) return;
+    Serial.println("err2");
+    if (getLidarStatus())
+    {
+        actionHandle.setState(ActionHandle::STATE::PAUSED);
+    }
+    Serial.println("err3");
+    actionHandle.actionLoop();
+    delay(250);
+}
+#endif
